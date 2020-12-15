5d15a7414a015ee6c415882de04d41b2ffee0d93
IvanOrtizp <ivanortizp4@gmail.com>
2020-12-15 14:57:43 +0530

    add rspec test for multiple dates

--- decidim-debates/spec/system/multiple_date_spec.rb
+++ decidim-debates/spec/system/multiple_date_spec.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 # frozen_string_literal_true
 
 require "spec_helper"
 
 describe "show multiple dates", type: :system do
-    include_context "with a component"
-    let(:manifest_name) { "debates" }
+  include_context "with a component"
+  let(:manifest_name) { "debates" }
 
-    let!(:debate) { create(:debate, component: component) }
+  let!(:debate) { create(:debate, component: component) }
 
-    before do
-      visit_component
-      click_link debate.title[I18n.locale.to_s], class: ".card__content--multiple"
+  before do
+    visit_component
+    click_link debate.title[I18n.locale.to_s], class: "card__content--multiple"
+  end
 
-    end
-
-    context "show date" do
-      it "shows the date" do
-        within ".card__content--multiple" do
-          expect(page).to have_content(debate.start_time.strftime("%d"))
-          expect(page).to have_content(debate.start_time.strftime("%H:%M"))
-        end
-      end
-
-      it "shows the extra date" do
-        within ".card__content--multiple" do
-          expect(page).to have_content(debate.end_time.strftime("%d"))
-          expect(page).to have_content(debate.end_time.strftime("%H:%M"))
+  context "show date" do
+    it "shows the date" do
+      within ".card__content--multiple" do
+        expect(page).to have_content(debate.start_time.strftime("%d"))
+        expect(page).to have_content(debate.start_time.strftime("%H:%M"))
       end
     end
+
+    it "shows the extra date" do
+      within ".card__content--multiple" do
+        expect(page).to have_content(debate.end_time.strftime("%d"))
+        expect(page).to have_content(debate.end_time.strftime("%H:%M"))
+      end
+    end
+  end
 end
