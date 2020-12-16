107a7d5ecd1b53fc374b1633f563ced6a0e5a86c
Ivan Ortiz <72499955+IvanOrtizp@users.noreply.github.com>
2020-12-16 11:54:20 +0100

    Update multiple_date_spec.rb

--- decidim-debates/spec/system/multiple_date_spec.rb
+++ decidim-debates/spec/system/multiple_date_spec.rb
@@ -10,14 +10,14 @@ describe "show multiple dates", type: :system do
 
   context "when shows the debate component" do
     it "shows the start date" do
-      within "extra__date" do
+      within ".extra__date" do
         expect(page).to have_content(debate.start_time.strftime("%d"))
         expect(page).to have_content(debate.start_time.strftime("%H:%M"))
       end
     end
 
     it "shows the end date" do
-      within "extra__date" do
+      within ".extra__date" do
         expect(page).to have_content(debate.end_time.strftime("%d"))
         expect(page).to have_content(debate.end_time.strftime("%H:%M"))
       end
