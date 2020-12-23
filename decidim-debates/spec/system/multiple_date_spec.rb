f7c2ff9b6f18ee07b0a390daba86d6db6e90ae53
IvanOrtizp <ivanortizp4@gmail.com>
2020-12-23 17:24:42 +0530

    add the rspec test for multiple dates

--- decidim-debates/spec/system/multiple_date_spec.rb
+++ decidim-debates/spec/system/multiple_date_spec.rb
@@ -13,16 +13,10 @@ describe "show multiple dates", versioning: true, type: :system do
     visit_component
     click_link debate.title[I18n.locale.to_s], class: "card__link"
   end
-  
-  context "when start date and end date are different" do
+
+  context "when start and end on different day" do
     let(:start_time) { Time.current - 1.day }
-
-    it "compare the dates" do
-      within ".extra__date" do
-        expect(debate.start_time.strftime("%d%m")).not_to eq(debate.end_time.strftime("%d%m"))
-      end
-
-      it "shows the start and end date" do
+      it "shows two days" do
         within ".extra__date" do
           expect(page).to have_content(debate.start_time.strftime("%d"))
           expect(page).to have_content(debate.start_time.strftime("%H:%M"))
@@ -30,56 +24,45 @@ describe "show multiple dates", versioning: true, type: :system do
           expect(page).to have_content(debate.end_time.strftime("%H:%M"))
         end
       end
-    end
-
-    context "when start date and end date are the same" do
-      it "compare the days" do
+  
+      it "shows two dates" do
         within ".extra__date" do
-          expect(debate.start_time.strftime("%m%Y")).to eq(debate.end_time.strftime("%m%Y"))
-          expect(debate.start_time.strftime("%H")).to be < debate.end_time.strftime("%H")
-        end
-      end
-
-      it "show the day" do
-        within ".extra__date" do
-          expect(page).to have_content(debate.start_time.strftime("%d"))
-          expect(page).to have_content(debate.start_time.strftime("%H:%M"))
-          expect(page).to have_content(debate.end_time.strftime("%H:%M"))
+          expect(debate.start_time.strftime("%d%m")).not_to eq(debate.end_time.strftime("%d%m"))
         end
       end
     end
+  context "when start and end date are the same" do
+    it "comprove the hours" do
+      within ".extra__date" do
+        expect(debate.start_time.strftime("%H")).to be < debate.end_time.strftime("%H")
+        expect(debate.start_time.strftime("%m%Y")).to eq(debate.end_time.strftime("%m%Y"))
+      end
+    end
 
-    context "when start and end day has the same but the month are different"
+    it "shows the hours" do
+      within ".extra__date" do
+        expect(page).to have_content(debate.start_time.strftime("%d"))
+        expect(page).to have_content(debate.start_time.strftime("%H:%M"))
+        expect(page).to have_content(debate.end_time.strftime("%H:%M"))
+      end
+    end
+  end
+  context "when start and end on same day but different month" do
+  let(:start_time) { Time.current - 1.month }
 
-    it "compare two dates" do
+    it "shows two days" do
       within ".extra__date" do
         expect(debate.start_time.strftime("%d")).to eq(debate.end_time.strftime("%d"))
         expect(debate.start_time.strftime("%m")).not_to eq(debate.end_time.strftime("%m"))
       end
     end
 
-    it "shows two days" do
-      within ".card_content" do
+    it "shows two dates" do
+      within ".extra__date" do
         expect(page).to have_content(debate.start_time.strftime("%d"))
         expect(page).to have_content(debate.start_time.strftime("%H:%M"))
         expect(page).to have_content(debate.end_time.strftime("%d"))
         expect(page).to have_content(debate.end_time.strftime("%H:%M"))
-      end
-    end
-  end
-
-  context "when there is no end date" do
-    it "show only start date" do
-      within ".extra__date" do
-        expect(page).not_to have_content(debate.end_time.strftime("%d"))
-        expect(page).to have_content(debate.start_time.strftime("%H:%M"))
-      end
-    end
-
-    it "doesn't have end date" do
-      within ".extra__date" do
-        expect(page).not_to have_content(debate.end_time.strftime("%d"))
-        expect(page).not_to have_content(debate.end_time.strftime("%H:%M"))
       end
     end
   end
